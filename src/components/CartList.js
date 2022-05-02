ffee8ad3fe27172791c4a81c81ccf1a4a85cb21e
timand1 <80768929+timand1@users.noreply.github.com>
2022-05-02 16:06:02 +0200

    Header, cartlist
    Add functions to add/remove products
    Added totalcost
    
    Still need to fix discount and css
    
    End of day update

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,8 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addProduct, removeProduct, addDiscount } from '../redux/actions/productAction';
+
 export default function CartList(props) {
-    const { product } = props;
+    const { product, id } = props;
+    const productList = useSelector(state => state.products);
+
+    const dispatch = useDispatch();
+
+    const uniqueProduct = productList.filter(product => product.id === id);
+    let amount = uniqueProduct.length;
+
+    let discountAmount = 0;
+
+    if (productList.find(product => product.title === 'Bryggkaffe') && productList.find(product => product.title === 'Gustav Adolfsbakelse')) {
+        discountAmount = discountAmount + 1
+        // dispatch(addDiscount(discountAmount))
+        console.log(discountAmount)
+    }
+
+    function removeCart() {
+        dispatch(removeProduct(product))
+    }
+
+    function addCart() {
+        dispatch(addProduct(product))
+    }
+
     return (
-        <section>
+        <section id={product.id} >
             <p>{product.title}</p>
+            <p>{product.price * amount} kr</p>
+            <p>{amount}</p>
+            <button onClick={addCart}>ADD</button>
+            <button onClick={removeCart} >REMOVE</button>
         </section>
     )
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,19 +1,28 @@
 const initialState = {
-    products: []
+    products: [],
+    totalCost: 0
 }
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_PRODUCT':
             return {
-                products: [...state.products, action.payload]
+                products: [...state.products, action.payload],
+                totalCost: state.totalCost + action.payload.price
             }
         case 'REMOVE_PRODUCT':
+            const productIndex = state.products.findIndex(product => product.id === action.payload.id);
             const copyProductsArray = [...state.products];
-            const newProductsArray = copyProductsArray.filter(product => product.id !== action.payload)
+            copyProductsArray.splice(productIndex, 1)
+            // const newProductsArray = copyProductsArray.filter(product => product.id !== action.payload);
             return {
-
-                products: newProductsArray
+                products: copyProductsArray,
+                totalCost: state.totalCost - action.payload.price
+            }
+        case 'ADD_DISCOUNT':
+            return {
+                ...state,
+                totalCost: state.totalCost - (38 * action.payload)
             }
         default:
             return state
