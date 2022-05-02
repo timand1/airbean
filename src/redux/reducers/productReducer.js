27b565eb1727ecd56a33d1d4d08512933392567f
timand1 <80768929+timand1@users.noreply.github.com>
2022-05-02 13:09:32 +0200

    Products
    Added fetch for all products
    Redux actions and reducers

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -4,7 +4,19 @@ const initialState = {
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'ADD_PRODUCT':
+            return {
+                products: [...state.products, action.payload]
+            }
+        case 'REMOVE_PRODUCT':
+            const copyProductsArray = [...state.products];
+            const newProductsArray = copyProductsArray.filter(todo => todo.id !== action.payload)
+            return {
 
+                products: newProductsArray
+            }
+        default:
+            return state
     }
 }
 
