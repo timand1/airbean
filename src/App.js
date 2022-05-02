8092bb1d7ceec39d09257be187c9671465526fb0
timand1 <80768929+timand1@users.noreply.github.com>
2022-05-02 11:54:46 +0200

    Start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Landing from './views/Landing';
+import About from './views/About';
+import Menu from './views/Menu';
+import Nav from './views/Nav';
+import Status from './views/Status';
+import ErrorPage from './views/ErrorPage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/nav' element={<Nav />} />
+        <Route path='/status' element={<Status />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
