f391ff5e469aa1f782b2fdbfede5d075dbeec901
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-05-03 09:40:42 +0200

    landing css finished

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -2,13 +2,17 @@ import './Landing.css';
 import { useNavigate } from 'react-router-dom';
 
 import landing from '../assets/graphics/airbean-landing.svg';
+import graphicsLeft from '../assets/graphics/intro-graphic-left.svg';
+import graphicsRight from '../assets/graphics/intro-graphic-right.svg';
 
 function Landing() {
     const navigate = useNavigate();
 
     return (
-        <section className='landing-section'>
-            <img src={landing} alt="AirBean-logo" onClick={ () => navigate('/menu')} />
+        <section className='landing-section' onClick={ () => navigate('/menu')}>
+            <img src={graphicsLeft} className="leaves leaves--left" alt="leaves" />
+            <img src={landing} className="landing__logo" alt="AirBean-logo"  />
+            <img src={graphicsRight} className="leaves leaves--right" alt="leaves" />
         </section>
     )
 }
