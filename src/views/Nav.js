3e0ac30b93205a368e2d82d0452408ba7bbc89fe
timand1 <80768929+timand1@users.noreply.github.com>
2022-05-03 10:31:51 +0200

    Slide out nav

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import close from '../assets/graphics/close.svg';
 import './Nav.css';
 
@@ -9,22 +10,29 @@ import './Nav.css';
 function Nav() {
 
     const navigate = useNavigate();
-   
+    const [slide, setSlide] = useState(false)
+
+    function navSlide() {
+        setSlide(!slide)
+        setTimeout(() => {
+            navigate(-1)
+        }, 500);
+    }
 
     return (
-        <section className='flex nav-container'>
+        <section className={slide ? 'flex nav-container slide-out' : 'flex nav-container'}>
             <div className='flex close-btn-container'>
-                <button className='close-btn' onClick={() => navigate(-1)} style={{ backgroundImage: `url(${close})` }} ></button>
+                <button className='close-btn' onClick={navSlide} style={{ backgroundImage: `url(${close})` }} ></button>
             </div>
 
-            <div className='flex menu-btn-container'>              
-                
-                <button className='menu-btn'onClick={ () => navigate('/menu')}>Meny</button>
+            <div className='flex menu-btn-container'>
+
+                <button className='menu-btn' onClick={() => navigate('/menu')}>Meny</button>
                 <div className='border'></div>
-                <button className='menu-btn' onClick={ () => navigate('/about')}>Vårt kaffe</button>
-                <div className='border'></div>            
-                <button className='menu-btn' onClick={ () => navigate('/status')} >Orderstatus</button>
-                <div className='border'></div>  
+                <button className='menu-btn' onClick={() => navigate('/about')}>Vårt kaffe</button>
+                <div className='border'></div>
+                <button className='menu-btn' onClick={() => navigate('/status')} >Orderstatus</button>
+                <div className='border'></div>
             </div>
         </section>
     )
